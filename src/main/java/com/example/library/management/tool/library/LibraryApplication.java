ae5f105abd81d633196effba755825b2ab1f366a
krishnak03 <krishnaverma0522@gmail.com>
2024-05-30 00:39:45 +0530

    Adding Cors Configuration in main application

--- src/main/java/com/example/library/management/tool/library/LibraryApplication.java
+++ src/main/java/com/example/library/management/tool/library/LibraryApplication.java
@@ -2,6 +2,10 @@ package com.example.library.management.tool.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LibraryApplication {
@@ -10,4 +14,20 @@ public class LibraryApplication {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+	@Configuration
+	public static class WebConfig {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("*") // allows all origins , if need specific -- add here eg- http://localhost:4200/
+							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+							.allowedHeaders("*");
+				}
+			};
+		}
+	}
 }
